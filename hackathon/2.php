4776fe12ce980224cbe0d64bfbfda74e846c2c12
mahesh456789 <nagamaheshkavuri@gmail.com>
2024-06-26 21:39:01 +0530

    first commit

---
+++ hackathon/2.php
@@ -0,0 +1,17 @@
+
+<div class="comment">
+  <h4><?php echo $data['name']; ?></h4>
+  
+  <p><?php echo $data['comment']; ?></p>
+  <?php $reply_id = $data['id']; ?>
+  <button class="reply" onclick = "reply(<?php echo $reply_id; ?>, '<?php echo $data['name']; ?>');">Reply</button>
+  <?php
+  unset($datas);
+  $datas = mysqli_query($conn, "SELECT * FROM tb_data WHERE reply_id = $reply_id");
+  if(mysqli_num_rows($datas) > 0) {
+    foreach($datas as $data){
+      require '3.php';
+    }
+  }
+  ?>
+</div>
